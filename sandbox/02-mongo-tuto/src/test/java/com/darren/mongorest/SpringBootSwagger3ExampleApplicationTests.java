5f90c5e6c1e84c2d79bec6acd6c0f49efcbea523
Darren Ong <onglityit@entomo.co-at-372321201642>
2023-04-19 09:16:13 +0800

    fix: refactor package, and ready for mongo

---
+++ sandbox/02-mongo-tuto/src/test/java/com/darren/mongorest/SpringBootSwagger3ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darren.mongorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwagger3ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
