4377eed17c8bae6cb83a16163b1467b22631e717
Darren Ong <onglityit@entomo.co-at-372321201642>
2023-04-19 19:41:47 +0800

    feat(wip): abe to compile

--- sandbox/03-jpa/src/test/java/com/darren/mongorest/GroceryServiceTests.java
+++ sandbox/03-jpa/src/test/java/com/darren/mongorest/GroceryServiceTests.java
@@ -1,9 +1,18 @@
 package com.darren.mongorest;
 
+import com.darren.mongorest.model.GroceryItem;
 import com.darren.mongorest.repo.GroceryItemRepository;
+import com.darren.mongorest.service.GroceryService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 
 
@@ -11,15 +20,31 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
-class GroceryServiceTests {
+public class GroceryServiceTests {
 
+	static String generatedUuid;
+
+	@BeforeAll
+	static void setup(){
+		generatedUuid = UUID.randomUUID().toString();
+	}
+
+	@Test
+	void contextLoads() {
+	}
 
 
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void givenGroceryItemRepository_whenInsertGroceryItem_shouldFindGroceryItem(){
+		GroceryItemRepository groceryItemRepository = mock(GroceryItemRepository.class);
+
+
 	}
 
 }
