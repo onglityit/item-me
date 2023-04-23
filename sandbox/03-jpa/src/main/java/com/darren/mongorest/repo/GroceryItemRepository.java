de1596646c662132cbc6b1320c8b5cd7dc68e83b
Darren Ong <onglityit@entomo.co-at-372321201642>
2023-04-23 20:37:22 +0700

    feat: find landlord by id,  remove unused

--- sandbox/03-jpa/src/main/java/com/darren/mongorest/repo/GroceryItemRepository.java
+++ sandbox/03-jpa/src/main/java/com/darren/mongorest/repo/GroceryItemRepository.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public interface GroceryItemRepository extends MongoRepository<GroceryItem, String> {
 
-    @Query("{name:'?0'}")
-    GroceryItem findItemByName(String name);
+    //@Query("{name:'?0'}")
+    //GroceryItem findItemByName(String name);
     @Query("{id:'?0'}")
     GroceryItem findItemById(String id);
 
-    @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
-    List<GroceryItem> findAll(String category);
+    //@Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+    //List<GroceryItem> findAll(String category);
 
     //public long count();
 
