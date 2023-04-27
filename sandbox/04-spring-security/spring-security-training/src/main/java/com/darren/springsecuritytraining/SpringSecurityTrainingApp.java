e1a9b744f1ae80257c2b558331c3a6e4fc64708f
Darren Ong <onglityit@entomo.co>
2023-04-27 17:11:02 +0800

    feat(wip): spring security basics

---
+++ sandbox/04-spring-security/spring-security-training/src/main/java/com/darren/springsecuritytraining/SpringSecurityTrainingApp.java
@@ -0,0 +1,12 @@
+package com.darren.springsecuritytraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTrainingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTrainingApp.class, args);
+    }
+}
